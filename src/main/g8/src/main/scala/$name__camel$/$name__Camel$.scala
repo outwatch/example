f4a92c1acf96190ca5f84f1685533e8e6a5b3a58
johannes karoff <johannes@karoff.net>
2020-03-23 10:14:42 +0100

    fix build

--- src/main/g8/src/main/scala/$name__camel$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$name__camel$/$name__Camel$.scala
@@ -5,6 +5,8 @@ import outwatch.dsl._
 
 import cats.effect.IO
 
+object $name;format="Camel"$ {
+
   def main(args: Array[String]): Unit = {
 
     OutWatch.renderInto[IO]("#app", h1("Hello World")).unsafeRunSync()
